ce21fa497a6c621bf4131b84e8ae9ffed8bee3b2
Ankita <ankita39640@gmail.com>
2022-06-25 19:11:24 +0530

    this pointer

---
+++ tut49.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+    //     int a;
+    //     public:
+    //     void setData(int a){
+    //         this->a=a;
+    //     }
+    //     void getData(){
+    //         cout<<"The value of a is "<<a<<endl;
+    //     }
+    // };
+    int a;
+
+public:
+    A &setData(int a)
+    {
+        this->a = a;
+        return *this;
+    }
+    void getData()
+    {
+        cout << "The value of a is " << a << endl;
+    }
+};
+int main()
+{
+    A a;
+    a.setData(4).getData();
+    // a.getData();
+    return 0;
+}
